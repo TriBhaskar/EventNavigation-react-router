b9d45d19175e70c3f71e7780226e64a491906189
TriBhaskar <panthribhaskar@gmail.com>
2024-02-01 20:41:52 +0530

    Update styling and add navigation links

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,3 +1,16 @@
+import { Link, useParams } from "react-router-dom";
+
 export default function EventDetailPage() {
-  return <h1>Event Detail Page</h1>;
+  const param = useParams();
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>{param.eventId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
 }
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,3 +1,39 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  {
+    id: "e1",
+    title: "First Event",
+    description: "This is the first event!",
+  },
+  {
+    id: "e2",
+    title: "Second Event",
+    description: "This is the second event!",
+  },
+  {
+    id: "e3",
+    title: "Third Event",
+    description: "This is the third event!",
+  },
+  {
+    id: "e4",
+    title: "Fourth Event",
+    description: "This is the fourth event!",
+  },
+];
+
 export default function EventsPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {EVENTS.map((e) => (
+          <li key={e.id}>
+            <Link to={e.id}>{e.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
