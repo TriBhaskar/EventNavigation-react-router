fb69a3b371c0f25e177f60f9ca7dd477bbdaea66
TriBhaskar <panthribhaskar@gmail.com>
2024-02-01 22:09:04 +0530

    Refactor EventsPage to fetch events from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,20 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { path: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                //
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,39 +1,13 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const EVENTS = [
-  {
-    id: "e1",
-    title: "First Event",
-    description: "This is the first event!",
-  },
-  {
-    id: "e2",
-    title: "Second Event",
-    description: "This is the second event!",
-  },
-  {
-    id: "e3",
-    title: "Third Event",
-    description: "This is the third event!",
-  },
-  {
-    id: "e4",
-    title: "Fourth Event",
-    description: "This is the fourth event!",
-  },
-];
-
-export default function EventsPage() {
+function EventsPage() {
+  const fetchedEvents = useLoaderData();
   return (
     <>
-      <h1>Events Page</h1>
-      <ul>
-        {EVENTS.map((e) => (
-          <li key={e.id}>
-            <Link to={e.id}>{e.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={fetchedEvents} />
     </>
   );
 }
+
+export default EventsPage;
