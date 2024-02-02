3862c0488e1567526c3b9bf13e40cbddfebfc28f
TriBhaskar <panthribhaskar@gmail.com>
2024-02-02 21:19:17 +0530

    Refactor EventsList and EventsPage components

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,6 +1,8 @@
-import classes from './EventsList.module.css';
+import { useLoaderData } from "react-router-dom";
+import classes from "./EventsList.module.css";
 
-function EventsList({ events }) {
+function EventsList() {
+  const events = useLoaderData();
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
