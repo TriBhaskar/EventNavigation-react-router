ce2603f32b637b69433ed290426d175fd99e90de
TriBhaskar <panthribhaskar@gmail.com>
2024-02-03 21:41:25 +0530

    undoing some changes

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -2,12 +2,12 @@ import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootPage() {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading...</p>}
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
