39a2c983e5dc15a8c6d191d7542a0cceb140629d
TriBhaskar <panthribhaskar@gmail.com>
2024-02-05 20:56:33 +0530

    Add EventDetailPage loader and update EventsList link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,12 @@ import RootPage from "./pages/Root";
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
-
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 const router = createBrowserRouter([
   {
@@ -46,7 +26,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader, // This is a function that will be called to load data for the page
           },
-          { path: ":eventId", element: <EventDetailPage /> },
+          {
+            path: ":eventId",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,11 +1,11 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const param = useParams();
+  const data = useLoaderData(); // useLoaderData() converts response to objects (from the loader function)
   return (
     <>
-      <h1>Event Detail Page</h1>
-      <p>{param.eventId}</p>
+      <EventItem event={data.event} />
       <p>
         <Link to=".." relative="path">
           Back
@@ -13,4 +13,19 @@ export default function EventDetailPage() {
       </p>
     </>
   );
+}
+
+export async function loader({ request, params }) {
+  // This function will be called to load data for the page (when the page is being navigated to)
+  const id = params.eventId; // This is the ID from the URL path
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
 }
