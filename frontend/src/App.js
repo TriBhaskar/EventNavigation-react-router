24173bcd5b9130732b7e4a0079d46653ec4e50d3
TriBhaskar <panthribhaskar@gmail.com>
2024-02-06 21:01:47 +0530

    added new event functionality with post req to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
 
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,3 +1,29 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 export default function NewEventPage() {
-  return <h1>New Event Page</h1>;
+  return <EventForm />;
+}
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not send event." }, { status: 500 });
+  }
+
+  return redirect("/events");
 }
