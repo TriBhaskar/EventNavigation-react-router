fefad5773e74f4f5eacfc8c831e50fe133da7958
TriBhaskar <panthribhaskar@gmail.com>
2024-02-10 14:40:11 +0530

    Controlling which data should be deferred

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,31 @@
-import { Link, json, redirect, useRouteLoaderData } from "react-router-dom";
+import {
+  Await,
+  Link,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventDetailPage() {
   // const data = useLoaderData(); // useLoaderData is a hook that returns the data loaded by the loader for the current page
-  const data = useRouteLoaderData("event-detail"); // useRouteLoaderData is a hook that returns the data loaded by the loader by the given ID
-  console.log(data);
+  const { event, events } = useRouteLoaderData("event-detail"); // useRouteLoaderData is a hook that returns the data loaded by the loader by the given ID
+
   return (
     <>
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{ textAlign: "center" }}>LOADING.....</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>LOADING.....</p>}>
+        <Await resolve={events}>
+          {(loadEvents) => <EventsList events={loadEvents} />}
+        </Await>
+      </Suspense>
       <p>
         <Link to=".." relative="path">
           Back
@@ -17,9 +35,7 @@ export default function EventDetailPage() {
   );
 }
 
-export async function loader({ request, params }) {
-  // This function will be called to load data for the page (when the page is being navigated to)
-  const id = params.eventId; // This is the ID from the URL path
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
   if (!response.ok) {
     throw json(
@@ -28,9 +44,32 @@ export async function loader({ request, params }) {
     );
   } else {
     const data = await response.json();
-    console.log(data);
-    return data;
+    return data.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Failed to fetch events" };
+    // throw new Response(JSON.stringify({ message: "Failed to fetch events" }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Failed to fetch events" }, { status: 500 }); // json() is a helper function that returns a response object with a JSON body
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  // This function will be called to load data for the page (when the page is being navigated to)
+  const id = params.eventId; // This is the ID from the URL path
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ request, params }) {
